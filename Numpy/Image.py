1a5e57ecb80754d7dce509b5c427640e477fa797
rrsalian <rrsalian16@gmail.com>
2020-04-25 22:33:52 +0530

    commit :25-04-2020

--- Numpy/Image.py
+++ Numpy/Image.py
@@ -1,3 +1,14 @@
-import cv
+""" import cv
 im = cv.imread("image.jpg", mode='RGB')
-print(type(im))
+print(type(im)) """
+
+# load and show an image with Pillow
+from PIL import Image
+# Open the image form working directory
+image = Image.open('image.jpg')
+# summarize some details about the image
+print(image.format)
+print(image.size)
+print(image.mode)
+# show the image
+load_image.show()
